04f8b4fa2ef13f81037afb91e27536b50608b62a
rojonaitor <bryanlundberg@outlook.com>
2023-08-15 12:11:15 -0600

    add elements for event view

---
+++ src/components/CombatDetails.jsx
@@ -0,0 +1,52 @@
+
+
+export default function CombatDetails({}) {
+  return (
+    <>
+      <div className="container-combat-details">
+        <div className="card-player">
+          <div className="player-info">
+            <div className="player-info-title">Killer</div>
+            <div className="player-info-title">Mephistooo [KAPHABAL]</div>
+          </div>
+          <div className="player-set">
+            <img src="#" alt="killer bag"></img>
+            <img src="#" alt="killer helmet"></img>
+            <img src="#" alt="killer cape"></img>
+            <img src="#" alt="killer main-weapon"></img>
+            <img src="#" alt="killer robe"></img>
+            <img src="#" alt="killer offhand"></img>
+            <img src="#" alt="killer food"></img>
+            <img src="#" alt="killer shoes"></img>
+            <img src="#" alt="killer potion"></img>
+            <img src="#" alt="killer mount"></img>
+          </div>
+        </div>
+        <div className="card-player">
+          <div className="player-info">
+            <div className="player-info-title">Killer</div>
+            <div className="player-info-title">Mephistooo [KAPHABAL]</div>
+          </div>
+          <div className="player-set">
+            <img src="#" alt="killer bag"></img>
+            <img src="#" alt="killer helmet"></img>
+            <img src="#" alt="killer cape"></img>
+            <img src="#" alt="killer main-weapon"></img>
+            <img src="#" alt="killer robe"></img>
+            <img src="#" alt="killer offhand"></img>
+            <img src="#" alt="killer food"></img>
+            <img src="#" alt="killer shoes"></img>
+            <img src="#" alt="killer potion"></img>
+            <img src="#" alt="killer mount"></img>
+          </div>
+        </div>
+        <div className="container-inventory">
+          <div className="intentory-title">Victim's Inventory</div>
+          <div className="inventory-stack">
+            <img src="#" alt="#"></img>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
