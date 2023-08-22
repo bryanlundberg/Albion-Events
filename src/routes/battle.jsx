87af9dd45cf206f628c2edd386a4a9e5570c08e1
rojonaitor <bryanlundberg@outlook.com>
2023-08-21 21:32:09 -0600

    generate killboard state

--- src/routes/battle.jsx
+++ src/routes/battle.jsx
@@ -1,14 +1,37 @@
-import { useLoaderData } from "react-router-dom"
+import { Link, useLoaderData } from "react-router-dom"
 import { API } from "../const/api"
+import Header from "../components/Header"
+import { useEffect, useState } from "react"
+
+// 861067253
 
 export default function Battle() {
-  const { battle, history } = useLoaderData()
+  const { battle } = useLoaderData()
+  const [killboard, setKillboard] = useState([])
+  const [killsOffset, setPlayersOffset] = useState(0)
+
   console.log(battle)
-  console.log(history)
+
+  useEffect(() => {
+    if (killsOffset <= battle.totalKills) {
+      fetch(`${API.BATTLES.HISTORY}${battle.id}?offset=${killsOffset}&limit=51`)
+        .then(response => response.json())
+        .then(result => {
+          console.log(result)
+          setKillboard(killboard.concat(result))
+          setPlayersOffset(killsOffset+51)
+        })
+    }
+  })
 
   return (
     <>
-    hi
+      <Header title={'BATTLE REPORT'} />
+      <Link to="/" >return index</Link>
+
+      <div>
+        {killboard.length}
+      </div>
     </>
   )
 }
@@ -17,10 +40,7 @@ export async function loader({ params }) {
   try {
     const request = await fetch(`${API.BATTLES.OVERALL}${params.battleId}`)
     const battle = await request.json()
-    // remove this search (history) later making it dynamic with useEffect
-    const request2 = await fetch(`${API.BATTLES.HISTORY}${params.battleId}?offset=350&limit=1`)
-    const history = await request2.json()
-    return { battle, history }
+    return { battle }
   } catch (error) {
     console.error(error)
     return { battle: null}
