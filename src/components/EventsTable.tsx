bbbd7691f301497c98f5ea64b1789bc44a0c2f76
rojonaitor <bryanlundberg@outlook.com>
2024-05-11 16:16:54 -0600

    fix key main table rows

--- src/components/EventsTable.tsx
+++ src/components/EventsTable.tsx
@@ -1,5 +1,4 @@
 import getEvents from "@/loaders/getEvents";
-import { OverallEvent } from "@/interfaces/OverallEvent";
 import LinkLabel from "./LinkItem";
 import Link from "next/link";
 
@@ -20,40 +19,36 @@ export default async function EventsTable() {
         <tbody>
           {lastEvents &&
             lastEvents.length >= 1 &&
-            lastEvents.map((event: OverallEvent) => {
-              return (
-                <>
-                  <tr key={event.BattleId + event.EventId + event.Killer.Id}>
-                    <td className="text-start hidden sm:block">
-                      {event.TimeStamp.slice(5, 10) +
-                        " " +
-                        event.TimeStamp.slice(11, 16)}
-                    </td>
-                    <td className="text-center max-w-14  overflow-ellipsis overflow-hidden">
-                      <LinkLabel
-                        label={event.Killer.Name}
-                        href={`/players/${event.Killer.Id}`}
-                      />
-                    </td>
-                    <td className="text-center w-32">
-                      {event.Killer.AverageItemPower.toFixed(0)} vs{" "}
-                      {event.Victim.AverageItemPower.toFixed(0)}
-                    </td>
-                    <td className="text-center max-w-14  overflow-ellipsis overflow-hidden">
-                      <LinkLabel
-                        label={event.Victim.Name}
-                        href={`/players/${event.Victim.Id}`}
-                      />
-                    </td>
-                    <td className="text-end">
-                      <Link href={`/events/${event.EventId}`}>
-                        {event.TotalVictimKillFame.toLocaleString()}
-                      </Link>
-                    </td>
-                  </tr>
-                </>
-              );
-            })}
+            lastEvents.map((event: OverallEvent) => (
+              <tr key={`${event.BattleId}-${event.EventId}-${event.Killer.Id}`}>
+                <td className="text-start hidden sm:block">
+                  {event.TimeStamp.slice(5, 10) +
+                    " " +
+                    event.TimeStamp.slice(11, 16)}
+                </td>
+                <td className="text-center max-w-14  overflow-ellipsis overflow-hidden">
+                  <LinkLabel
+                    label={event.Killer.Name}
+                    href={`/players/${event.Killer.Id}`}
+                  />
+                </td>
+                <td className="text-center w-32">
+                  {event.Killer.AverageItemPower.toFixed(0)} vs{" "}
+                  {event.Victim.AverageItemPower.toFixed(0)}
+                </td>
+                <td className="text-center max-w-14  overflow-ellipsis overflow-hidden">
+                  <LinkLabel
+                    label={event.Victim.Name}
+                    href={`/players/${event.Victim.Id}`}
+                  />
+                </td>
+                <td className="text-end">
+                  <Link href={`/events/${event.EventId}`}>
+                    {event.TotalVictimKillFame.toLocaleString()}
+                  </Link>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </>
