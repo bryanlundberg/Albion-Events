13eb46dc36e9837ef4458911b5f9cebb85271eaf
rojonaitor <bryanlundberg@outlook.com>
2024-05-12 06:00:05 -0600

    style: select

--- src/components/FilterActivity.tsx
+++ src/components/FilterActivity.tsx
@@ -11,7 +11,11 @@ export default function FilterActivity({
     <div className="flex justify-between items-center">
       <p>{player.Name}&apos;s last activity: </p>
       <div className="filter-container">
-        <select value={filter} onChange={(e) => onChangeFilter(e.target.value)}>
+        <select
+          value={filter}
+          onChange={(e) => onChangeFilter(e.target.value)}
+          className="w-28 border border-gray-200 p-1 "
+        >
           <option value="all">All</option>
           <option value="kill">Kill</option>
           <option value="death">Death</option>
